9743d5c8c44a8d33cb1fb7c924db713cbf0414b7
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-04 23:09:16 +0530

    Conditional statements in python

--- day3/conditional.py
+++ day3/conditional.py
@@ -1,5 +1,6 @@
 name = "anuj"
 
+#Comparison operators
 if(name == "anuj"):
     print("yes")
 else:
@@ -7,9 +8,17 @@ else:
 
 age = 22
 
+#Branching
 if(age > 18):
     print("you can enter")
 elif(age == 18):
     print("go see admin")
 else:
     print("you can not enter")
+
+#Logic operators (or,and, not)
+if(name == "anuj" or age == 23):
+    print("correct")
+
+if(name != "anu" and age != 23):
+    print("correct")
