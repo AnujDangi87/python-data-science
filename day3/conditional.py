20d4fc29d474cdbd1cf32186d0be273f44f9808a
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-04 21:35:11 +0530

    Conditional statements in python

---
+++ day3/conditional.py
@@ -0,0 +1,15 @@
+name = "anuj"
+
+if(name == "anuj"):
+    print("yes")
+else:
+    print("no")
+
+age = 22
+
+if(age > 18):
+    print("you can enter")
+elif(age == 18):
+    print("go see admin")
+else:
+    print("you can not enter")
