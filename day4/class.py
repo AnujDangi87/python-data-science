e08a46437fc4af6a0292034f379884d402f21f39
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-05 22:52:18 +0530

    Adding classes and object to day4

---
+++ day4/class.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+#class classname(parent class):
+class Circle(object):
+
+    #constructor 
+    def __init__(self, radius, color):
+        self.radius = radius
+        self.color = color
+    #self -> self referencing to current obj
+    
+    def get_area(self):
+        return 3.14*self.radius*self.radius
+    
+    def drawCircle(self):
+        plt.gca().add_patch(plt.Circle((0, 0), radius=self.radius, fc=self.color))
+        plt.axis('scaled')
+        plt.show()
+    
+
+obj = Circle(2, "red")
+print(obj.radius, obj.color)
+print(obj.get_area())
+obj.drawCircle()
